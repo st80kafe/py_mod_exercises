6405cc0904f4a2dd2d5a62e4202fd9cb814b71fd
ptowr <peter@storyfu.com>
2019-05-16 06:55:26 -0700

    using // operator

--- digits.py
+++ digits.py
@@ -1,13 +1,20 @@
 def digits(t):
-    '''prints digits of integer t to length 9 digits
-    accuracy of last digit printed is sometimes -1'''
-    for e in range(1,10):
-        if abs(t)/10**e < 1:
-            if abs(t)/10**e > .1:
-                d = (abs(t)/10**e)
-                for ee in range(e,0,-1):
-                    print(int(d*10))
-                    d = (abs(d*10.0) - int(d*10))
+    '''prints digits of positive integer t to length 9 digits'''
+
+
+    for c in range(1,10):   # find number of digits
+
+        if t/10**c < 1:
+
+            if t/10**c > .1:  # number of digits = c
+
+                for e in range(c-1,-1,-1):
+                # value of exponent e before division of int t by 10**e
+                
+                    print(t//10**e) # print first digit of t
+
+                    # subtract value of first digit multiplied by power of 10
+                    t = t - ( (t//10**e)*10**e )
                         
                                 
 
